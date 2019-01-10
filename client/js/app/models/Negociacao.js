a70527893a5589bce44ca44181613509ff4915d0
João Paulo <jpmf1995@hotmail.com>
2019-01-10 17:10:03 -0200

    criando a classe necociacao

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,20 @@
+class Negociacao{
+  constructor(data, quantidade, valor){
+    this._data = new Date(data.getTime());
+    this._quantidade = 1;
+    this._valor = 0.0;
+    Object.freeze(this);
+  }
+  get volume(){
+    return this._quantidade * this._valor;
+  }
+  get data(){
+    return new Date(this._data.getTime());
+  }
+  get uantidade(){
+    return this._quantidade;
+  }
+  get valor(){
+    return this._valor;
+  }
+}
