5046db5ded203e9ad8aeb3b476e085d749204e0f
João Paulo <jpmf1995@hotmail.com>
2019-01-11 16:50:58 -0200

    criação da classe NegociacaoController

--- client/js/controllers/NegociacaoController.js
+++ client/js/controllers/NegociacaoController.js
@@ -2,6 +2,11 @@ class NegociacaoController{
 
   adiciona(event){
     event.preventDefault();
-    alert('Chamei a negociacao');
+    let inputData = document.querySelector('#data');
+    let inputQunatidade = document.querySelector('#quantidade');
+    let inputValor = document.querySelector('#valor');
+
+
+
   }
 }
