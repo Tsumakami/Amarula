61610a04cebc93036a61e2ac5e99f222d0dce77f
João Paulo <jpmf1995@hotmail.com>
2019-01-14 14:15:17 -0200

    Corrigindo o controlador de Negociaçoes

--- client/js/controllers/NegociacaoController.js
+++ client/js/controllers/NegociacaoController.js
@@ -3,7 +3,7 @@ class NegociacaoController{
   constructor(){
     let $ = document.querySelector.bind(document);
     this._inputData = $('#data');
-    this._inputQunatidade = $('#quantidade');
+    this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
   }
 
@@ -11,14 +11,20 @@ class NegociacaoController{
   adiciona(event){
     event.preventDefault();
 
+    let data = new Date(
+	...this._inputData.value
+		.split('-')
+		.map((item,indice)=> item - indice % 2)
+	);
 
-    let negociacao = new Negociacao(
-      let data = new Date(this._inputData.value.replace(/-/g, ','));
-      this._inputQunatidade.value,
-      this._inputValor.value
-    );
+	let negociacao = new Negociacao(
+		data,
+		this._inputQuantidade.value,
+		this._inputValor.value
+	);
 
 
+	console.log(negociacao);
 
 
   }
