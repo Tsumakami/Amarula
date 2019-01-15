c29df71088f1e689cd42a7bb968c60050cde32ae
João Paulo <jpmf1995@hotmail.com>
2019-01-15 16:31:17 -0200

    criando lista de negociacao

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -3,9 +3,9 @@ class ListaNegociacoes {
     this._negociacoes = [];
   }
   adiciona(negociacao){
-
+  	this._negociacoes.push(negociacao); 
   }
   get negociacoes(){
-    return [].concat(this.negociacoes);
+    return [].concat(this._negociacoes);
   }
 }
